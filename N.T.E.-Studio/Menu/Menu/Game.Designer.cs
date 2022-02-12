df7ed3c74ced82cc3b5ed790f26597680fedaf79
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2022-02-12 23:11:43 +0100

    Add files via upload
    zkrášlení menu

---
+++ N.T.E.-Studio/Menu/Menu/Game.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Menu
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.snake_picBox = new System.Windows.Forms.PictureBox();
+            this.start_button = new System.Windows.Forms.Button();
+            this.score_lbl = new System.Windows.Forms.Label();
+            this.highscore_lbl = new System.Windows.Forms.Label();
+            this.tick_timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.snake_picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // snake_picBox
+            // 
+            this.snake_picBox.BackColor = System.Drawing.Color.LightGreen;
+            this.snake_picBox.Location = new System.Drawing.Point(75, 44);
+            this.snake_picBox.Name = "snake_picBox";
+            this.snake_picBox.Size = new System.Drawing.Size(624, 624);
+            this.snake_picBox.TabIndex = 0;
+            this.snake_picBox.TabStop = false;
+            this.snake_picBox.Paint += new System.Windows.Forms.PaintEventHandler(this.snake_picBox_Paint);
+            // 
+            // start_button
+            // 
+            this.start_button.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_button.Location = new System.Drawing.Point(742, 44);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(192, 86);
+            this.start_button.TabIndex = 1;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // score_lbl
+            // 
+            this.score_lbl.AutoSize = true;
+            this.score_lbl.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score_lbl.Location = new System.Drawing.Point(739, 177);
+            this.score_lbl.Name = "score_lbl";
+            this.score_lbl.Size = new System.Drawing.Size(65, 18);
+            this.score_lbl.TabIndex = 2;
+            this.score_lbl.Text = "Score: ";
+            // 
+            // highscore_lbl
+            // 
+            this.highscore_lbl.AutoSize = true;
+            this.highscore_lbl.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.highscore_lbl.Location = new System.Drawing.Point(739, 212);
+            this.highscore_lbl.Name = "highscore_lbl";
+            this.highscore_lbl.Size = new System.Drawing.Size(99, 18);
+            this.highscore_lbl.TabIndex = 3;
+            this.highscore_lbl.Text = "Highscore: ";
+            // 
+            // tick_timer
+            // 
+            this.tick_timer.Tick += new System.EventHandler(this.tick_timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(765, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 781);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.highscore_lbl);
+            this.Controls.Add(this.score_lbl);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.snake_picBox);
+            this.Name = "Game";
+            this.Text = "Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Game_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.snake_picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox snake_picBox;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Label score_lbl;
+        private System.Windows.Forms.Label highscore_lbl;
+        private System.Windows.Forms.Timer tick_timer;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ N.T.E.-Studio/Menu/Menu/Menu.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Menu
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.play = new System.Windows.Forms.Button();
+            this.options = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // play
+            // 
+            this.play.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.play.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.play.Location = new System.Drawing.Point(339, 169);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(158, 59);
+            this.play.TabIndex = 0;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            this.play.MouseEnter += new System.EventHandler(this.play_MouseEnter);
+            this.play.MouseLeave += new System.EventHandler(this.play_MouseLeave);
+            // 
+            // options
+            // 
+            this.options.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.options.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.options.Location = new System.Drawing.Point(339, 234);
+            this.options.Name = "options";
+            this.options.Size = new System.Drawing.Size(158, 59);
+            this.options.TabIndex = 1;
+            this.options.Text = "Options";
+            this.options.UseVisualStyleBackColor = true;
+            this.options.Click += new System.EventHandler(this.options_Click);
+            this.options.MouseEnter += new System.EventHandler(this.options_MouseEnter);
+            this.options.MouseLeave += new System.EventHandler(this.options_MouseLeave);
+            // 
+            // exit
+            // 
+            this.exit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.exit.Location = new System.Drawing.Point(370, 299);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(87, 39);
+            this.exit.TabIndex = 2;
+            this.exit.Text = "Exit";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            this.exit.MouseEnter += new System.EventHandler(this.exit_MouseEnter);
+            this.exit.MouseLeave += new System.EventHandler(this.exit_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(321, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "N.T.E. studio";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(333, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Snake game";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(840, 503);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.options);
+            this.Controls.Add(this.play);
+            this.Name = "Menu";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button play;
+        private System.Windows.Forms.Button options;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
